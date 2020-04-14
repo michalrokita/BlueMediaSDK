2d8604a3af1d20502f5b7359a01a10f14e46ce1f
Michal Rokita <michal.rokita17@gmail.com>
2020-04-14 14:57:25 +0200

    Amount bugfix

--- src/DataTypes/Transactions/Amount.php
+++ src/DataTypes/Transactions/Amount.php
@@ -6,6 +6,17 @@ namespace michalrokita\BlueMediaSDK\DataTypes\Transactions;
 
 class Amount
 {
+    /**
+     * @var float
+     */
+    private $amount;
     public function __construct(float $amount)
-    {}
+    {
+        $this->amount = $amount;
+    }
+
+    public function __toString(): string
+    {
+        return (string)$this->amount;
+    }
 }
